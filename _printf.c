e8b63ab0f15b06b7d371de4bf18169ece5c207f6
KARRACH <karrach.abdellah.dev@gmail.com>
2023-04-25 19:32:05 +0000

    fix task0

--- _printf.c
+++ _printf.c
@@ -21,11 +21,7 @@ else
 switch (format[i])
 {
 case 'c':
-j = va_arg(args, int);
-if (j > 0)
-count += print_char(j);
-if (j < 0)
-count += print_string("(null)");
+count += print_char(va_arg(args, int));
 break;
 case 's':{
 s = va_arg(args, char *);
@@ -41,6 +37,7 @@ case '%':
 count += print_char('%');
 break;
 default:{
+count += print_char('%');
 count += print_char(format[i]);
 break; }}}}
 va_end(args);
